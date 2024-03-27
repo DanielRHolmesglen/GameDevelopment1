a26649a88ba173b3e03287e1e0eaa6ae3ba91bca
DanielRHolmesglen <daniel.roberts@holmesglen.edu.au>
2024-03-27 17:30:52 +1100

    player health minor changes

--- Assets/Scripts/DamageSystem/PlayerHealth.cs
+++ Assets/Scripts/DamageSystem/PlayerHealth.cs
@@ -17,6 +17,22 @@ public class PlayerHealth : Health
     private void Start()
     {
         lastDamagedBy = playerNum; //default this number to the players number. This means that if the player is killed by an environment object it will subtract points from itself;
+        anim.SetBool("Dead", false);
     }
+    public override void Die()
+    {
+        base.Die();
+        anim.SetBool("Dead", true);
 
+        //update scores
+        if (lastDamagedBy == playerNum) GamePlayManager.instance.UpdateScore(playerNum, -1); //if the last player to damage this player was itself, then reduce its score by 1
+        else GamePlayManager.instance.UpdateScore(lastDamagedBy, 1); //else, if the last player to damage this player was someone else, add to their score
+
+        //make the character respawn, but on a delay so that the animation can play out
+        StartCoroutine(RespawnCharacterOnDelay(playerNum));
+    }
+    IEnumerator RespawnCharacterOnDelay(int playerNumber)
+    {
+        yield return null;
+    }
 }
