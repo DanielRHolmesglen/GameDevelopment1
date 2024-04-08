aad683a977c301653e9cd5a292f265e0e8c6d995
DanielRHolmesglen <daniel.roberts@holmesglen.edu.au>
2024-04-08 14:24:50 +1000

    fov started

---
+++ Assets/Scripts/FOV Mechanic/FieldOfView.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FieldOfView : MonoBehaviour
+{
+    public float radius;
+    [Range(0,360)]
+    public float viewAngle;
+
+    public Vector3 DirFromAngle(float angleInDegrees, bool angleIsGlobal)
+    {
+        if (!angleIsGlobal)
+        {
+            angleInDegrees += transform.eulerAngles.y;
+        }
+        return new Vector3(Mathf.Sin(angleInDegrees * Mathf.Deg2Rad), 0, Mathf.Cos(angleInDegrees * Mathf.Deg2Rad));
+    }
+    private void OnDrawGizmos()
+    {
+        Vector3 lhAngle = DirFromAngle(-viewAngle / 2, true);
+        Vector3 rhAngle = DirFromAngle(viewAngle / 2, true);
+
+
+    }
+}
